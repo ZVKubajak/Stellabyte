24c74736fc2f94077002438bfbe484db04b5d6af
VanceGoblin <zvkubajak@gmail.com>
2025-01-24 12:27:53 -0600

    Created Boilerplate server.ts

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import cors from "cors";
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(process.cwd(), "../client/dist")));
+
+app.get("*", (_req, res) => {
+  res.sendFile(path.join(process.cwd(), "..client/dist/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}.`);
+});
