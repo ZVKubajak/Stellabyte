8b1af61e871e81c465e5676ac4c80243bee91b88
VanceGoblin <zvkubajak@gmail.com>
2025-01-24 16:39:30 -0600

    Edited Prisma Schema & Created createFile

--- server/src/controllers/fileController.ts
+++ server/src/controllers/fileController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import AWS from "../config/awsConfig";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const prisma = new PrismaClient();
+const s3 = new AWS.S3();
+const bucket = process.env.BUCKET_NAME!;
 
 export const getAllFiles = async (_req: Request, res: Response) => {
   try {
@@ -10,6 +15,7 @@ export const getAllFiles = async (_req: Request, res: Response) => {
       select: {
         id: true,
         userId: false,
+        fileName: true,
         fileType: true,
         fileSize: true,
         s3Url: false,
@@ -38,6 +44,7 @@ export const getUserFiles = async (req: Request, res: Response) => {
       select: {
         id: true,
         userId: false,
+        fileName: true,
         fileType: true,
         fileSize: true,
         s3Url: true,
@@ -67,6 +74,7 @@ export const getFileById = async (req: Request, res: Response) => {
       select: {
         id: true,
         userId: false,
+        fileName: true,
         fileType: true,
         fileSize: true,
         s3Url: true,
@@ -80,6 +88,48 @@ export const getFileById = async (req: Request, res: Response) => {
     }
   } catch (error) {
     console.error("Error fetching file by ID:", error);
+    res.status(500).json({ message: "Server Error", error });
+  }
+};
+
+export const createFile = async (req: Request, res: Response) => {
+  const { userId } = req.params;
+
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file uploaded." });
+    }
+
+    const bucketParams = {
+      Bucket: bucket,
+      Key: `${userId}/${req.file.originalname}`,
+      Body: req.file.buffer,
+      ContentType: req.file.mimetype,
+    };
+
+    const data = await s3.upload(bucketParams).promise();
+
+    const newFile = await prisma.file.create({
+      data: {
+        userId,
+        fileName: req.file.originalname,
+        fileType: req.file.mimetype,
+        fileSize: req.file.size,
+        s3Url: data.Location,
+      },
+      select: {
+        id: true,
+        userId: false,
+        fileName: true,
+        fileType: true,
+        fileSize: true,
+        s3Url: true,
+      },
+    });
+
+    res.status(201).json({ message: "File created successfully.", newFile });
+  } catch (error) {
+    console.error("Error uploading file:", error);
     res.status(500).json({ message: "Server Error", error });
   }
 };
