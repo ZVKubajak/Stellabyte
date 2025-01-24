7de609cfe4cdb855e491dd4703758956f31e31a7
berczikbryce <bryceberczik.dev@gmail.com>
2025-01-24 15:48:21 -0600

    added createUserSchema

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -6,17 +6,56 @@ const prisma = new PrismaClient();
 const saltRounds = 10;
 
 export const getUsers = async (req: Request, res: Response) => {
-    try {
-        const users = await prisma.user.findMany({
-            select: {
-                id: true,
-                email: true,
-                myFiles: true
-            }
-        });
+  try {
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        myFiles: true,
+      },
+    });
 
-        res.status(200).json(users);
-    } catch (error) {   
-        res.status(500).json({ message: "Internal server error" });
-    }
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        myFiles: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    const hashedPassword = bcrypt.hash(password, saltRounds);
+    const user = await prisma.user.create({
+      data: { email, password: hashedPassword },
+      select: {
+        id: true,
+        email: true,
+        myFiles: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    res.status(201).json({ message: "User created successfully", user });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
