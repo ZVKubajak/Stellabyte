da3f85ac737c8576dd1e52ebb50e12e41092f604
berczikbryce <bryceberczik.dev@gmail.com>
2025-01-25 11:46:20 -0600

    added proxy for /api

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
