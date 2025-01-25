9db3f918e4b926cad7de2c48e781f3114b3fba2d
VanceGoblin <zvkubajak@gmail.com>
2025-01-25 14:52:06 -0600

    Created fileService.ts

---
+++ client/src/services/fileService.ts
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+export const getAllFiles = async () => {
+  try {
+    const response = await axios.get(`http://localhost:3001/api/files`);
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching all files:", error);
+    throw error;
+  }
+};
+
+export const getUserFiles = async (userId: string) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3001/api/files/user/${userId}`
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching files by userId:", error);
+    throw error;
+  }
+};
+
+export const getFileById = async (id: string) => {
+  try {
+    const response = await axios.get(
+      `http://localhost:3001/api/files/id/${id}`
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching file by id:", error);
+    throw error;
+  }
+};
+
+export const uploadFile = async (file: File, userId: string) => {
+  try {
+    const data = new FormData();
+    data.append("file", file);
+    data.append("userId", userId);
+
+    const response = await axios.post(`http://localhost:3001/api/files`, data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error uploading file:", error);
+    throw error;
+  }
+};
+
+export const removeFile = async (id: string, userId: string) => {
+  try {
+    const response = await axios.delete(
+      `http://localhost:3001/api/files/${id}`,
+      {
+        data: { userId },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error removing file:", error);
+    throw error;
+  }
+};
--- server/src/routes/api/fileRoutes.ts
+++ server/src/routes/api/fileRoutes.ts
@@ -13,7 +13,7 @@ const router = express.Router();
 router.get("/", getAllFiles);
 router.get("/user/:id", getUserFiles);
 router.get("/id/:id", getFileById);
-router.post("/:id", upload.single("file"), uploadFile);
+router.post("/", upload.single("file"), uploadFile);
 router.delete("/:id", removeFile);
 
 export { router as fileRouter };
