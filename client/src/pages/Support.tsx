38e60c2db74169a531af394853eaf361504c21fa
VanceGoblin <zvkubajak@gmail.com>
2025-01-27 16:45:14 -0600

    Redid onSubmit Logic & Added React Hook Form

--- client/src/pages/Support.tsx
+++ client/src/pages/Support.tsx
@@ -1,69 +1,69 @@
-import { useState, useEffect } from "react";
-import { jwtDecode } from "jwt-decode";
+import { useState } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import auth from "../utils/auth";
 import Swal from "sweetalert2";
 
+const supportSchema = z.object({
+  name: z.string().max(50, "Name can not be more than 50 letters."),
+  email: z.string().email("Invalid email."),
+  message: z.string().max(1000, "Message can not be more than 1000 letters."),
+});
+
+type TSupportSchema = z.infer<typeof supportSchema>;
+
 const Support = () => {
-  const [result, setResult] = useState("");
   const [hasEmail, setHasEmail] = useState(true);
-  const [userEmail, setUserEmail] = useState<string | null>(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem("id_token");
-    if (token) {
-      try {
-        const decoded: { email: string } = jwtDecode(token);
-        setUserEmail(decoded.email);
-      } catch (error) {
-        console.error("Invalid token:", error);
-      }
-    }
-  }, []);
+  let userEmail = "";
+  const profile = auth.getProfile();
+  if (profile) userEmail = profile.email;
 
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<TSupportSchema>({
+    resolver: zodResolver(supportSchema),
+  });
 
-    const form = event.currentTarget;
-
-    setResult("Sending....");
-    const formData = new FormData(form);
-
-    if (hasEmail && userEmail) {
-      formData.set("email", userEmail);
-    }
-
-    formData.append("access_key", "73a6d637-0dd7-4625-ba1a-3c217cac240a");
-
+  const onSubmit = async (data: TSupportSchema) => {
     try {
+      const webForm = new FormData();
+      webForm.append("name", data.name);
+      webForm.append("email", data.email);
+      webForm.append("message", data.message);
+      webForm.append("access_key", "73a6d637-0dd7-4625-ba1a-3c217cac240a");
+
       const response = await fetch("https://api.web3forms.com/submit", {
         method: "POST",
-        body: formData,
+        body: webForm,
       });
 
-      const data = await response.json();
+      const parsedResponse = await response.json();
 
-      if (data.success) {
-        setResult("Form Submitted Successfully");
-        form.reset();
+      if (parsedResponse.success) {
+        reset();
         Swal.fire({
           title: "Success!",
           text: "Form Submitted Successfully.",
           icon: "success",
         });
       } else {
-        console.error("Error", data);
-        setResult(data.message);
+        console.error("Web3Form Error:", parsedResponse);
         Swal.fire({
-          title: "Error!",
-          text: data.message,
+          title: "An error has occurred!",
+          text: "Please try again later.",
           icon: "error",
         });
       }
     } catch (error) {
-      console.error("Error submitting the form:", error);
-      setResult("An error occurred. Please try again.");
+      console.error("onSubmit Error:", error);
       Swal.fire({
-        title: "Error!",
-        text: "An error occurred. Please try again.",
+        title: "An error has occurred!",
+        text: "Please try again later.",
         icon: "error",
       });
     }
@@ -105,7 +105,9 @@ const Support = () => {
             </label>
             {hasEmail && userEmail ? (
               <div className="flex flex-row items-center justify-between border border-[whitesmoke] rounded p-2">
-                <span className="text-[15px] text-[whitesmoke]">{userEmail}</span>
+                <span className="text-[15px] text-[whitesmoke]">
+                  {userEmail}
+                </span>
                 <button
                   type="button"
                   className="text-[15px] text-[whitesmoke] underline ml-4"
