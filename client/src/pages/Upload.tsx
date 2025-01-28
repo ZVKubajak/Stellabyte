608b54fe068c5eb4f256bd0d3f98d125ed933071
VanceGoblin <zvkubajak@gmail.com>
2025-01-28 15:02:37 -0600

    Live Session Commit #1

--- client/src/pages/Upload.tsx
+++ client/src/pages/Upload.tsx
@@ -1,27 +1,39 @@
+import { uploadFile } from "../services/fileService";
+import auth from "../utils/auth";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileArrowUp } from "@fortawesome/free-solid-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 
+let userId = "";
+const profile = auth.getProfile();
+if (profile) {
+  userId = profile.id;
+} else {
+  throw new Error("Failed retireving user.");
+}
+
+const onSubmit = () => {};
+
 const Upload = () => {
   return (
     <div>
       <div className="text-center mt-[150px]">
         <h1 className="text-[whitesmoke]">Upload Your Files</h1>
-        <FontAwesomeIcon
-          icon={faFileArrowUp}
-          className="text-[whitesmoke] text-6xl my-4"
-        />
-      </div>
-
-      <div>
-        <Form className="border">
-          <Form.Group className="flex justify-between border px-8">
-            <Form.Control type="file" />
-            <FontAwesomeIcon icon={faCheck} />
-          </Form.Group>
-        </Form>
+        <div className="flex flex-row">
+          <div>
+            <Form className="border">
+              <Form.Group className="flex justify-between border px-8">
+                <Form.Control type="file" />
+                <FontAwesomeIcon icon={faCheck} />
+              </Form.Group>
+            </Form>
+          </div>
+          <div>
+            <FontAwesomeIcon icon={faFileArrowUp} />
+          </div>
+        </div>
       </div>
     </div>
   );
