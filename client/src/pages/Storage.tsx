6993ba05f914abdbf05ffdfa6daea2d11e0d098d
VanceGoblin <zvkubajak@gmail.com>
2025-01-30 13:11:40 -0600

    Created File Card Layout

--- client/src/pages/Storage.tsx
+++ client/src/pages/Storage.tsx
@@ -1,7 +1,19 @@
 const Storage = () => {
   return (
-    <div>
-      <div></div>
+    <div className="my-32">
+      <h1 className="text-center text-[whitesmoke] text-5xl">Your Files</h1>
+
+      <div className="mt-20 space-y-8">
+        <div className="bg-white border-2 border-gray-500 rounded-lg h-16 w-3/4 p-2 mx-auto">
+          File Card
+        </div>
+        <div className="bg-white border-2 border-gray-500 rounded-lg h-16 w-3/4 p-2 mx-auto">
+          File Card
+        </div>
+        <div className="bg-white border-2 border-gray-500 rounded-lg h-16 w-3/4 p-2 mx-auto">
+          File Card
+        </div>
+      </div>
     </div>
   );
 };
