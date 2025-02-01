0aa694573d980fb816d1e6e3ca437ea3417d2406
VanceGoblin <zvkubajak@gmail.com>
2025-01-31 18:22:40 -0600

    Formatted Backend Code

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,9 @@
 import { Router } from "express";
 import apiRoutes from "./api/index";
-// import { authenticateToken } from "../utils/auth";
+import { authenticateToken } from "../utils/auth";
 
 const router = Router();
 
-router.use(
-  "/api",
-  // authenticateToken,
-  apiRoutes
-);
+router.use("/api", authenticateToken, apiRoutes);
 
 export default router;
