50adf3034ade484b5256a5a9926cb884f757c082
VanceGoblin <zvkubajak@gmail.com>
2025-07-04 08:37:41 -0500

    Refactored File Controller

--- server/src/config/awsConfig.ts
+++ server/src/config/awsConfig.ts
@@ -1,11 +1,23 @@
-import AWS from "aws-sdk";
-import dotenv from "dotenv";
+import { S3Client } from "@aws-sdk/client-s3";
 
-dotenv.config();
-
-AWS.config.update({
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY,
+const s3Client = new S3Client({
+  region: "us-east-1",
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY!,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY!,
+  },
 });
 
-export default AWS;
+export default s3Client;
+
+// import AWS from "aws-sdk";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// AWS.config.update({
+//   accessKeyId: process.env.ACCESS_KEY,
+//   secretAccessKey: process.env.SECRET_ACCESS_KEY,
+// });
+
+// export default AWS;
