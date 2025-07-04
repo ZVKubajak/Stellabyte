024e222b30753f109e1e677de4865d47736b85bd
VanceGoblin <zvkubajak@gmail.com>
2025-07-04 12:20:09 -0500

    Refactored Home, ErrorPage, Login, and Signup Pages

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloud } from "@fortawesome/free-solid-svg-icons";
-import Section from "../components/Section";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
-import auth from "../utils/auth";
+import Section from "../components/Section";
+import authToken from "../tokens/authToken";
 
 const Home = () => {
   const navigate = useNavigate();
+  const hasToken = !!authToken.get();
+
   return (
     <div className="flex flex-col lg:mx-[100px] xl:items-center 2xl:items-stretch">
       {/* first section */}
@@ -14,7 +16,8 @@ const Home = () => {
           <h1 className="text-[whitesmoke] mb-3 md:text-[38px] lg:text-[35px] lg:w-4/5 xl:text-[40px] xl:w-3/5 2xl:text-[50px]">
             Upload. Create. Illuminate Your Cloud Galaxy.
           </h1>
-          {auth.loggedIn() ? (
+
+          {hasToken ? (
             <button
               className="text-[whitesmoke] border p-2 rounded-xl cursor-pointer sm:text-lg md:text-xl lg:text-[20px] xl:text-[23px]"
               onClick={() => navigate("/upload")}
@@ -30,6 +33,7 @@ const Home = () => {
             </button>
           )}
         </div>
+
         <div className="flex">
           <FontAwesomeIcon
             className="testing text-[100px] text-[#1b2d47] drop-shadow-md opacity-80 blur-[0.5px] sm:text-[120px] md:text-[130px] xl:text-[135px] 2xl:text-[160px]"
@@ -46,12 +50,14 @@ const Home = () => {
           </h1>
           <Section text="Our app offers 50MB of free cloud storage for your important files and star maps, ensuring a secure and efficient experience at no cost." />
         </div>
+
         <div className="flex flex-col mt-[80px] xl:mt-0">
           <h1 className="text-center text-[whitesmoke] text-[24px] mb-[30px] sm:text-[27px]">
             Cloud Storage
           </h1>
           <Section text="Access your files anytime, anywhere with secure cloud storage. Retrieve your data effortlessly, from any device." />
         </div>
+
         <div className="flex flex-col mt-[80px] xl:mt-0">
           <h1 className="text-center text-[whitesmoke] text-[24px] mb-[30px] sm:text-[27px]">
             Easy to use
@@ -61,14 +67,13 @@ const Home = () => {
       </div>
 
       {/* third section */}
-
-      {!auth.loggedIn() ? (
+      {hasToken ? (
         <div className="flex flex-col items-center mb-[100px]">
           <h1 className="text-center text-[whitesmoke] text-[24px] mt-[80px] mb-[30px]">
             Join now
           </h1>
           <button
-            className="border border-[whitesmoke] w-1/2 border-[1px] text-[whitesmoke] p-2 rounded-xl cursor-pointer sm:text-xl sm:w-1/3 md:text-2xl xl:w-full 2xl:w-1/5"
+            className="border border-[whitesmoke] w-1/2 text-[whitesmoke] p-2 rounded-xl cursor-pointer sm:text-xl sm:w-1/3 md:text-2xl xl:w-full 2xl:w-1/5"
             onClick={() => navigate("/signup")}
           >
             Join and Upload
@@ -80,7 +85,7 @@ const Home = () => {
             Upload and Create
           </h1>
           <button
-            className="border border-[whitesmoke] w-1/2 border-[1px] text-[whitesmoke] p-2 rounded-xl cursor-pointer sm:text-xl sm:w-1/3 md:text-2xl xl:w-full 2xl:w-1/5"
+            className="border border-[whitesmoke] w-1/2 text-[whitesmoke] p-2 rounded-xl cursor-pointer sm:text-xl sm:w-1/3 md:text-2xl xl:w-full 2xl:w-1/5"
             onClick={() => navigate("/upload")}
           >
             Create Now
